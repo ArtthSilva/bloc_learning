0406e0fb4a58c6ea44f19d638847dd020d33a81a
ArtthSilva <arthur12pereira1@gmail.com>
2024-04-08 13:55:07 -0300

    feat: add equatable

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:bloc_learning/page_bloc/bloc/counter_bloc.dart';
 import 'package:bloc_learning/page_bloc/counter_bloc_page.dart';
 import 'package:bloc_learning/home_page.dart';
 import 'package:bloc_learning/page_cubit/cubit/counter_cubit.dart';
-import 'package:bloc_learning/page_cubit/cubit/counter_cubit_page.dart';
+import 'package:bloc_learning/page_cubit/counter_cubit_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/page_cubit/cubit/counter_cubit.dart
+++ lib/page_cubit/cubit/counter_cubit.dart
@@ -1,9 +1,10 @@
 
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState>{
-  CounterCubit() : super(CounterStateInitial());
+  CounterCubit() : super(const CounterStateInitial());
 
   void increment(){
     emit(CounterStateData(state.counter + 1));
